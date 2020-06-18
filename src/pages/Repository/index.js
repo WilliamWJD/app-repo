2b5e05edd60ce04fb76f10832c398e9cb6a7cfde
williamwjd <william.jose.wjd@gmail.com>
2020-06-18 15:18:43 -0300

    utilizando hooks ná página repository

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,99 +1,87 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import { Loading, Owner, IssueList } from './styles';
 import Container from '../../components/Container';
 
-export default class Repository extends Component {
-    state = {
-        repository: {},
-        issues: [],
-        loading: true,
-        filter: 'all',
-    };
+const Repository = (props) => {
+    const [repository, setRepository] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [filter, setFilter] = useState('all');
 
-    async componentDidMount() {
-        const { match } = this.props;
-        const repoName = decodeURIComponent(match.params.repository);
+    useEffect(() => {
+        async function loadRepo() {
+            const { match } = props;
+            const repoName = decodeURIComponent(match.params.repository);
 
-        const [repository, issues] = await Promise.all([
-            api.get(`/repos/${repoName}`),
-            api.get(`/repos/${repoName}/issues`, {
-                params: {
-                    state: 'open',
-                    per_page: 5,
-                },
-            }),
-        ]);
+            const [repo, issu] = await Promise.all([
+                api.get(`/repos/${repoName}`),
+                api.get(`/repos/${repoName}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5,
+                    },
+                }),
+            ]);
 
-        this.setState({
-            repository: repository.data,
-            issues: issues.data,
-            loading: false,
-        });
-    }
-
-    async componentDidUpdate(_, prevState) {
-        const { filter } = this.state;
-        const { match } = this.props;
-        const repoName = decodeURIComponent(match.params.repository);
-
-        if (prevState.filter !== filter) {
-            const response = await api.get(
-                `/repos/${repoName}/issues?state=${filter}`
-            );
-            console.log(response.data);
+            setRepository(repo.data);
+            setIssues(issu.data);
+            setLoading(false);
         }
-    }
+        loadRepo();
+    }, []);
 
-    render() {
-        const { repository, issues, loading, filter } = this.state;
+    return (
+        <>
+            {loading ? (
+                <Loading>Carregando</Loading>
+            ) : (
+                <Container>
+                    <Owner>
+                        <Link to="/">Voltar aos repositórios</Link>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <h1>{repository.name}</h1>
+                        <p>{repository.description}</p>
+                    </Owner>
 
-        if (loading) {
-            return <Loading>Carregando</Loading>;
-        }
+                    <select name="" id="">
+                        <option value="all">All</option>
+                        <option value="open">Open</option>
+                        <option value="closed">Closed</option>
+                    </select>
 
-        return (
-            <Container>
-                <Owner>
-                    <Link to="/">Voltar aos repositórios</Link>
-                    <img
-                        src={repository.owner.avatar_url}
-                        alt={repository.owner.login}
-                    />
-                    <h1>{repository.name}</h1>
-                    <p>{repository.description}</p>
-                </Owner>
+                    <IssueList>
+                        {issues.map((issue) => (
+                            <li key={String(issue.id)}>
+                                <img
+                                    src={issue.user.avatar_url}
+                                    alt={issue.user.login}
+                                />
+                                <div>
+                                    <strong>
+                                        <a href={issue.html_url}>
+                                            {issue.title}
+                                        </a>
+                                        {issue.labels.map((label) => (
+                                            <span key={String(label.id)}>
+                                                {label.name}
+                                            </span>
+                                        ))}
+                                    </strong>
+                                    <p>{issue.user.login}</p>
+                                </div>
+                            </li>
+                        ))}
+                    </IssueList>
+                </Container>
+            )}
+        </>
+    );
+};
 
-                <select name="" id="">
-                    <option value="all">All</option>
-                    <option value="open">Open</option>
-                    <option value="closed">Closed</option>
-                </select>
-
-                <IssueList>
-                    {issues.map((issue) => (
-                        <li key={String(issue.id)}>
-                            <img
-                                src={issue.user.avatar_url}
-                                alt={issue.user.login}
-                            />
-                            <div>
-                                <strong>
-                                    <a href={issue.html_url}>{issue.title}</a>
-                                    {issue.labels.map((label) => (
-                                        <span key={String(label.id)}>
-                                            {label.name}
-                                        </span>
-                                    ))}
-                                </strong>
-                                <p>{issue.user.login}</p>
-                            </div>
-                        </li>
-                    ))}
-                </IssueList>
-            </Container>
-        );
-    }
-}
+export default Repository;
